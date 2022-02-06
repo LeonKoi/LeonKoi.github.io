189abdb4e1666e2d8eebcb2d6112e50dca8830aa
LeonKoi <96500038+LeonKoi@users.noreply.github.com>
2022-02-07 02:02:00 +0300

    Залил для проверки 2

--- src/js/Site.Scripte.js
+++ src/js/Site.Scripte.js
@@ -1,18 +1,31 @@
 "use strict"
 
 
-  $("a[href^="#"]").click (function(){
+  $("a[href^='#']").click (function(){
     let valHref = $(this).attr("href");
-    $("html, body").animate({scrollTop: $(valHref).offset().top - 50+"px"});
+    $("html, body").animate({scrollTop: $(valHref).offset().top - 50+"px"});     /*якорные ссылки */
   
 });
 
 $(document).ready(function(){
 
+let options = {thresgold: [0.5]};
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = $('.title__container');
+  elements.each((i,el) => {
+  	 observer.observe(el);
+  });
+function onEntry (entry){
+	  entry.forEach(change => {
+	  	if (change.isIntersecting){
+	  		change.target.classList.add('show-anim');
+	  	}
+	  });
+}
 
 
+});
 
-
-
-
+$(document).ready(function() {
+  $('.image-link').magnificPopup({type:'image'}); /*Модальное окно */
 });
