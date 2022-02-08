f6e7ebb9a82011046226b4b2d3f6cc1386a5092c
LeonKoi <96500038+LeonKoi@users.noreply.github.com>
2022-02-08 13:56:17 +0300

    Залил для проверки

--- src/js/Site.Scripte.js
+++ src/js/Site.Scripte.js
@@ -51,9 +51,70 @@ function onEntry (entry){
 
 });
 
+/*Статистика 
+$('#lines1').animateNumber({ number: 120 });
+$('#lines2').animateNumber({ number: 4.600 });
+$('#lines3').animateNumber({ number: 340 });
+$('#lines4').animateNumber({ number: 23 });
+*/
+
+$(document).ready(function(){
+let options = {thresgold: [0.5]};
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = $('.GGGR');
+  elements.each((i,el) => {
+  	 observer.observe(el);
+  });
+function onEntry (entry){
+	  entry.forEach(change => {
+	  	if (change.isIntersecting){
+	  		change.target.classList.add('lines');
+	  	}
+	  });
+}
+
+
+});
+
+
+$(document).ready(function(){
+if ("typeee"=='opt1') {
+console.log("10000");
+alert ("Визитка");
+price += 1000;
+deadlines +=10;
+};
+if ("typeee"=='opt2') {
+console.log("2");
+alert ("Корпоративный");
+price += 1500;
+deadlines +=30;
+};
+if ('typeee'=='opt3') {
+console.log("3");
+alert ("Витрина");
+price += 2500;
+deadlines +=25;
+};
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
 function explode(){
  $(document).ready(function() {
-$("#exampleModal").modal('show');
+$("#exampleModal").modal('show');/*модельное окно таймер */
  });
 }
 setTimeout(explode, 7000);
